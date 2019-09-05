34e5d6a03bf2ef415e36d76db1c4cd87ddcfb556
RansomConant <ranmanconant@gmail.com>
2019-09-05 13:55:24 -0400

    Added GraphView lib, created test graph in humidity history activity

--- app/src/main/java/com/example/smarthiveapp/DisplayHumidityHistory.java
+++ app/src/main/java/com/example/smarthiveapp/DisplayHumidityHistory.java
@@ -6,16 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class DisplayHumidityHistory extends AppCompatActivity {
+
+    LineGraphSeries<DataPoint> series;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_humidity_history);
+
+
+        double x,y;
+        x = 0;
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        for(int i = 0; i < 750; i++){
+            x = x + 0.1;
+            y = Math.sin(x / 5) + 1;
+            series.appendData(new DataPoint(x,y), true, 750);
+        }
+        graph.addSeries(series);
     }
 
-    public void backHome(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
+
 }
